3f727a94e12001b944c36d65a7fe11eb2976ef21
Spirovanni <blackshieldsx@gmail.com>
2022-02-15 23:08:38 -0800

    Comment on definition

--- blockchain/faucet/migrations/2_faucet_migrations.js
+++ blockchain/faucet/migrations/2_faucet_migrations.js
@@ -1,7 +1,8 @@
 
 
+// eslint-disable-next-line no-undef
 const FaucetContract = artifacts.require("Faucet")
 
 module.exports = function (deployer) {
-    deployer.deploy(FaucetContract)
+  deployer.deploy(FaucetContract)
 }
